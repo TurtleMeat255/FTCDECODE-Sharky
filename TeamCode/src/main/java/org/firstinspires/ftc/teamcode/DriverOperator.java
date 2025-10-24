de9a788aa2b1de87b87cb57e5d458aa1a1232015
Coodingmaan <zephyrz080@stemia.ca>
2025-10-24 17:58:06 -0600

    Shooter implementation untested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOperator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOperator.java
@@ -4,11 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class DriverOperator extends LinearOpMode{
     FTCDriveTrain drivetrain = new FTCDriveTrain();
-
+    Shooter shooter = new Shooter();
     @Override
     public void runOpMode() {
         drivetrain.init(hardwareMap);
-
+        shooter.init(hardwareMap);
         waitForStart();
         while (opModeIsActive())
         {
@@ -16,9 +16,13 @@ public class DriverOperator extends LinearOpMode{
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
             boolean options = gamepad1.options;
+            boolean ShootButton = gamepad2.a;
+            boolean DownButton = gamepad2.x;
+            boolean UpButton = gamepad2.y;
 
             drivetrain.Translate(x,y,rx,options);
-
+            shooter.HoodStuff(UpButton, DownButton);
+            shooter.ShootStuff(ShootButton);
             // drivetrain translate stufffff
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -2,5 +2,46 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
 public class Shooter {
+DcMotor Shooter1;
+DcMotor Shooter2;
+Servo Hood1;
+Servo Hood2;
+boolean LastUp = false;
+boolean LastDown = false;
+double position = 0;
+public void init(HardwareMap hwMap) {
+    Shooter1 = hwMap.get(DcMotor.class, "Shooter1");
+    Shooter2 = hwMap.get(DcMotor.class, "Shooter2");
+    Hood1 = hwMap.get(Servo.class, "Hood1");
+    Hood2 = hwMap.get(Servo.class, "Hood2");
+    Shooter1.setDirection(DcMotorSimple.Direction.REVERSE);
+    Hood1.setDirection(Servo.Direction.REVERSE);
+    }
+    public void ShootStuff(boolean Shoot) {
+        if (Shoot) {
+        Shooter1.setPower(1);
+        } else {
+        Shooter1.setPower(0);
+        }
+    }
+    public void HoodStuff(boolean HoodUp, boolean HoodDown) {
+    if (HoodUp && !LastUp && position <= 0.675) {
+        position += 0.225;
+    }
+    if (HoodDown && !LastDown && position >= 0.225) {
+        position -= 0.225;
+    }
+
+
+    LastDown = HoodDown;
+    LastUp = HoodUp;
+    Hood1.setPosition(position);
+    Hood2.setPosition(position);
+    }
+
+
 }
