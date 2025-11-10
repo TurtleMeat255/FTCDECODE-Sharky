e4ecb4b9c9ddca7fd9036e55c229cc524cdc1c90
JPON24 <jpon24@shaw.ca>
2025-11-09 19:43:48 -0700

    limelight now detects april tags - thanks liam :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimeLight.java
@@ -4,12 +4,15 @@ import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
+import java.util.List;
 
+@TeleOp
 public class AprilTagLimeLight extends OpMode {
     private Limelight3A limeLight;
     private IMU Imu;
@@ -17,7 +20,7 @@ public class AprilTagLimeLight extends OpMode {
     @Override
     public void init() {
         limeLight = hardwareMap.get(Limelight3A.class, "limelight");
-        Imu = hardwareMap.get(IMU.class, "Imu");
+        Imu = hardwareMap.get(IMU.class, "imu");
         limeLight.pipelineSwitch(0); // ASK JACOB HOW TO FIX. IDK WHAT THIS IS. WE MIGHT NEED TWO FOR BOTH COLOR SENSING AND APRIL TAG DETECTION
         RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
@@ -37,20 +40,20 @@ public class AprilTagLimeLight extends OpMode {
         limeLight.updateRobotOrientation(orientation.getYaw());
         LLResult llresult = limeLight.getLatestResult();
 
-        LLResultTypes.FiducialResult fiducial = (LLResultTypes.FiducialResult) llresult.getFiducialResults();
+        List<LLResultTypes.FiducialResult> fiducial = llresult.getFiducialResults();
 
         if (llresult != null && llresult.isValid()) {
             Pose3D botPose = llresult.getBotpose_MT2();
             telemetry.addData("Tx", llresult.getTx());
             telemetry.addData("Ty", llresult.getTy());
             telemetry.addData("Target Area", llresult.getTa());
-            telemetry.addData("id", fiducial.getFiducialId());
+            telemetry.addData("id", fiducial.get(0).getFiducialId());
 
-            if (fiducial.getFiducialId() == 21) {
+            if (fiducial.get(0).getFiducialId() == 21) {
                 //run code
-            } else if (fiducial.getFiducialId() == 22) {
+            } else if (fiducial.get(0).getFiducialId() == 22) {
                 // run code
-            } else if (fiducial.getFiducialId() == 23) {
+            } else if (fiducial.get(0).getFiducialId() == 23) {
                 // run code
             }
 
